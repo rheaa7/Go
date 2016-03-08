bcd4cd12a8d0ac0df1406a992d23224d1e9a0460
Rheaa <rheaa@uw.edu>
2016-03-08 02:05:47 -0800

    woo

--- trie/trie.go
+++ trie/trie.go
@@ -10,71 +10,88 @@ type Trie struct {
 }
 
 type Node struct {
-    Letter rune
-    Children map[rune]*Node
+    Letter string
+    Children map[string]*Node
     WordEnd bool
     
 }
 
 func NewTrie() *Trie {
-    node := &Node{Children: make(map[rune]*Node)}
+    node := &Node{Children: make(map[string]*Node)}
     root := Trie{Root: node}
     return &root
 }
 
 func (trie *Trie) AddWord(entry string) {
     curr := trie.Root
-    for i, letter := range entry {
+    index := 0
+    for _, i := range entry {
+        letter := string(i)
+        index++
+        length := len(entry)
         // fmt.Println(curr)
         if curr.Children[letter] == nil {
-            var nodeMap = make(map[rune] *Node)
-            var node = Node{Letter: letter, Children: nodeMap}
-            
-            curr.Children[letter] = &node
-            //fmt.Println(curr.Children[letter])
-            
-            if i == len(entry) - 1 {
-                fmt.Println("what")
-                trie.Count++
-                curr.Children[letter].WordEnd = true
-                fmt.Println(curr);
+            if (index == length) {
+                var nodeMap = make(map[string] *Node)
+                curr.Children[letter] = &Node{Letter: letter, Children: nodeMap, WordEnd: true}
             } else {
-                curr.Children[letter].WordEnd = false
+                var nodeMap = make(map[string] *Node)
+                curr.Children[letter] = &Node{Letter: letter, Children: nodeMap, WordEnd: false}
             }
-            curr = curr.Children[letter]
+            // curr.Children[letter] = &node
+            // //fmt.Println(curr.Children[letter])
         }
+        if (index == length) {
+            curr.Children[letter].WordEnd = true
+        }
+        // curr = curr.Children[letter]
+        //     if i == len(entry) - 1 {
+        //         fmt.Println("what")
+        //         trie.Count++
+        //         curr.Children[letter].WordEnd = true
+        //         fmt.Println(curr);
+        //     } else {
+        //         curr.Children[letter].WordEnd = false
+        //     }
+        //     curr = curr.Children[letter]
+        // }
     }
     // fmt.Println(trie.Root)
 
 }
 
-func (trie *Trie) FindEntries(prefix string, max uint8) string {
+func (trie *Trie) FindEntries(prefix string, max uint8) []string {
     fmt.Println(trie.Count)
     currentNode := trie.Root
-    currentNode = make(map[rune] *Node)
+    // currentNode = make(map[rune] *Node)
     for i := range prefix {
         letter := string(i)
-        currentNode = currentNode.Children(letter)
+        currentNode = currentNode.Children[letter]
     }
-    // return FindEntriesHelper(max, uint8, word string, currentNode node) 
+    
+    listOfWords := trie.FindEntriesHelper(max, prefix, currentNode)
+    return listOfWords 
     //return "hello"
 }
 
-func FindEntriesHelper(max uint8, word string, currentNode *Node) {
-    var listOfWords []string
+func (trie *Trie) FindEntriesHelper(max uint8, word string, currentNode *Node) []string {
+    listOfWords := make([]string, 0)
     if (max == 0) {
-        return []listOfWords
+        return listOfWords
     } 
     if (currentNode.WordEnd) {
-        listOfWords = append.([]listOfWords, word)
-        fmt.Println([]listOfWords)
+        listOfWords = append(listOfWords, word)
+        fmt.Println(listOfWords)
     }
     if (currentNode.Children == nil) {
-        return []listOfWords
+        return listOfWords
     } else {
-        for i := range currentNode.Children {
-            currentNode = currentNode.Children[i]
-            return append([]listOfWords, FindEntriesHelper(max, (word + currentNode.child), currentNode.Children))
+        for letter, Children := range currentNode.Children {
+            currentNode = Children
+            fmt.Println(listOfWords)
+            // listOfWords = append(listOfWords, trie.FindEntriesHelper(max, (word + currentNode.Children), currentNode.Children)...)
+            listOfWords = append(listOfWords, trie.FindEntriesHelper(max, word + letter, currentNode)...)
         }
+        return listOfWords
     }
 }
